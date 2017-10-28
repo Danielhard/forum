66b4191bbefbe603ff8837afffcf875a26b72d01
lin <15010560780@163.com>
2017-10-28 11:22:16 +0800

    question

--- Forum/routes/sendQuestion.js
+++ Forum/routes/sendQuestion.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let question = require('../models/question');
+let userInfor = require('../models/userInfor');
 let router = express.Router();
 
 router.get('/', (req,res) => {
@@ -12,11 +13,25 @@ router.post("/subProblem",(req,res) => {
   let {username,title,content,time} = req.body;
   let tag = req.body['tag[]'];
   question.insertData('questions',{username,title,tag,content,time},function (err,result) {
+    console.log(result);
     if(err){
       res.send("问题未能成功发布");
+      return;
     }else if(title == ''||tag == ''|| content == ''){
       res.send("您有未填写项目，发布失败");
-    }else {res.send("问题发布成功");}
+      return;
+    }else {
+      var userId =  req.session.userId;
+      var questionId = result._id;
+      userInfor.updateData('userInfor',{userId},{$push : {'myquestion' : questionId}},null,function (err,result) {
+        if(err){
+          res.send("插入数据失败");
+          return;
+        }
+        console.log(result);
+        res.send("问题发布成功");
+      });
+      }
   });
 });
 
