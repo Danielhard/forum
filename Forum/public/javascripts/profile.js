d601eca383468aebfdb1f77ecdf689e4f53e26a3
dearclyuan <dearclyuan>
2017-10-28 11:55:47 +0800

    personal修改

--- Forum/public/javascripts/profile.js
+++ Forum/public/javascripts/profile.js
@@ -3,16 +3,37 @@
 	var doChange = document.querySelectorAll('.dochange');
 	var Changed = document.querySelectorAll('.changed');
 	var Oinput = document.querySelectorAll('input[name=contextdetail]');
-	console.log(Oinput);
+//	console.log(Oinput);
 
-	for(var i = 0; i < doChange.length; i++) {
-		(function(i){
-			doChange[i].addEventListener('touchstart',function() {
-			  this.style.display="none";
-			  console.log(i);
-			  Changed[i].style.display = "block";
-			})
-		})(i)
-	}
+//	for(var i = 0; i < doChange.length; i++) {
+//		(function(i){
+//			doChange[i].addEventListener('touchstart',function() {
+//			  this.style.display="none";
+//			  console.log(i);
+//			  Changed[i].style.display = "block";
+//			})
+//		})(i)
+//	}
+
+ $(".sbt").on("touchstart",function(e){
+ 	e.preventDefault();
+ 	
+ 	 var data={
+ 	   nickname:$(".nickname").val(),
+ 	   job:$('.job').val(),
+ 	   company:$('.company').val(),
+ 	   selfIntroduction:$('.selfIntroduction').val(),
+ 	   personalWebsite:$('.personalWebsite').val()
+ }
+ 	 
+ 	 console.log(data);
+ 	 $.post("/personalCenter/personalmessage",data,function(data){
+ 	    console.log(data);
+ })
+		 	
+ 	
+ })
+
+		
 	
 })()
